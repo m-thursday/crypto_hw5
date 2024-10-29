382f8deed52724cbae3fb7857424d82701eb8786
Max Thursby <maxthursby@gmail.com>
2024-10-29 00:14:10 -0500

    everything is updated just need to add 4(b)

--- tests/collision.py
+++ tests/collision.py
@@ -4,21 +4,33 @@ import random
 import time
 
 def messageGen(length):
-	characters = string.ascii_letters
-	return ''.join(secrets.choice(characters) for i in range (length))
+    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return ''.join(secrets.choice(characters) for _ in range(length))
 
+def runCollision(db):
+    m = messageGen(random.randrange(5, 15))
+    tmp = SHA256.new(m.encode())
+    mHash = tmp.digest()[:8]
+    db.append((m, mHash))
+    
+    for i in range(len(db)):
+        for j in range(i + 1, len(db)):  
+            if db[i][1] == db[j][1]:
+                print("Collision found:")
+                print("Message 1:", db[i][0], "Hash:", db[i][1])
+                print("Message 2:", db[j][0], "Hash:", db[j][1])
+                return True 
+                
+    return False 
 
 if __name__ == '__main__':
-	db = []
-	t = time.time()
-	random.seed(t)
-	while True:
-		m = messageGen(random.random())
-		tmp = SHA256.new(m)
-		mHash = tmp[:8]
-		db.append((m,mHash))
-		for i in range (len(db)):
-			for j in range (len(db)):
-				if db[i][1] == db[j][1]:
-					break
-		
+    x = 0
+    collision = False
+    db = []
+    t = time.time()
+    random.seed(t)
+    
+    while not collision:
+        print("Check", x)  
+        x += 1  
+        collision = runCollision(db)
